6dfd3929aa97fd81d1e8586f327a568efbc68b12
mrinalgaur2005 <mrinalgaur30@gmail.com>
2024-09-19 04:28:07 +0530

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Bot from "@/components/bot/bot";
+import { currentProfile } from "@/lib/current-user";
+import { initialProfile } from "@/lib/initial-profile";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+
+  const intialProfile = await initialProfile();
+
   return(
     <Bot/>
   );
