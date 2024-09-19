357a1c039b5b88318e72cb8be33234a0e51efa82
mrinalgaur2005 <mrinalgaur30@gmail.com>
2024-09-19 07:30:52 +0530

    ended

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Bot from "@/components/bot/bot";
-import { currentProfile } from "@/lib/current-user";
 import { initialProfile } from "@/lib/initial-profile";
-import { auth } from "@clerk/nextjs/server";
 
 export default async function Home() {
 
